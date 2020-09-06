d133a9338710026e5a9429f0985cca76aeff98d1
Burgentie <63803079+Burgentie@users.noreply.github.com>
2020-09-06 13:43:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ function play(guild, song) {
     .setDescription("**SONG**")
     .setColor("#23ff00")
     .addField("__**Song**__", `${song.title}`)
-    .addField("__**Song**__", `<@${user}>`)
+    .addField("__**Song**__", `<@${user.username}}>`)
     .setTimestamp()
 
     return serverQueue.textChannel.send(playingEmbed)
