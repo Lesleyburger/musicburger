0b7aee8eb1ed367d3db19bd12f77dfeef9df70f1
Burgentie <63803079+Burgentie@users.noreply.github.com>
2020-09-06 13:33:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -177,15 +177,14 @@ function play(guild, song) {
     })
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
 
-    var botEmbed = new discord.MessageEmbed()
+    var playingEmbed = new discord.MessageEmbed()
     .setDescription("**SONG**")
     .setColor("#23ff00")
     .addField("__**Song**__", `${song.title}`)
-    .addField("__**Playing in**__", message.guild.member(client.user).voice.channel)
     .addField("__**Added by:**__", `${member.user}`)
     .setTimestamp()
 
-    return serverQueue.textChannel.send(botEmbed)
+    return serverQueue.textChannel.send(playingEmbed)
 
 }
 
